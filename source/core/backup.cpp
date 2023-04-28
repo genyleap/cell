b8400886605182d72a4eb66fc448fe17b2657e95
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-04-28 17:04:45 +0330

    Add Backup.

---
+++ source/core/backup.cpp
@@ -0,0 +1,12 @@
+#ifdef __has_include
+# if __has_include("backup.hpp")
+#   include "backup.hpp"
+#else
+#   error "Cell's "backup.hpp" was not found!"
+# endif
+#endif
+
+CELL_NAMESPACE_BEGIN(Cell::Maintenance)
+
+
+CELL_NAMESPACE_END
---
+++ source/core/backup.hpp
@@ -0,0 +1,30 @@
+/*!
+ * @file        backup.hpp
+ * @brief       This file is part of the Cell Engine.
+ * @details     Backup manager.
+ * @author      <a href='https://www.kambizasadzadeh.com'>Kambiz Asadzadeh</a>
+ * @package     The Genyleap
+ * @since       29 Aug 2022
+ * @copyright   Copyright (c) 2022 The Genyleap. All rights reserved.
+ * @license     https://github.com/genyleap/cell/blob/main/LICENSE.md
+ *
+ */
+
+#ifndef CELL_BACKUP_HPP
+#define CELL_BACKUP_HPP
+
+#ifdef __has_include
+# if __has_include("common.hpp")
+#   include "common.hpp"
+#else
+#   error "Cell's "common.hpp" was not found!"
+# endif
+#endif
+
+CELL_NAMESPACE_BEGIN(Cell::Maintenance)
+
+
+CELL_NAMESPACE_END
+
+
+#endif // CELL_BACKUP_HPP
