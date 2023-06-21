c199fd6aaa8940c79bd33ab557b8bdf0aff61377
Kambiz Asadzadeh <kambiz.ceo@gmail.com>
2023-06-21 23:34:01 +0330

    Update
    - Added Router for WebServer module.

---
+++ source/modules/network/webserver/router.cpp
@@ -0,0 +1,112 @@
+#if __has_include("router.hpp")
+#   include "router.hpp"
+#else
+#   error "Cell's router was not found!"
+#endif
+
+#if __has_include("classes/mediatypes.hpp")
+#   include "classes/mediatypes.hpp"
+#else
+#   error "Cell's classes/mediatypes.hpp was not found!"
+#endif
+
+
+CELL_USING_NAMESPACE Cell;
+CELL_USING_NAMESPACE Cell::Types;
+
+CELL_NAMESPACE_BEGIN(Cell::Modules::BuiltIn::Network::WebServer)
+
+void Router::addRoute(const std::string& path, const Handler& handler, const std::string& method)
+{
+    std::string normalizedPath = normalizePath(path).value();
+    std::string methodKey = normalizeMethod(method).value();
+    m_routes[methodKey][normalizedPath] = handler;
+}
+
+void Router::addMiddleware(const Middleware& middleware)
+{
+    m_middleWares.push_back(middleware);
+}
+
+Response Router::routeRequest(const Request& request)
+{
+    auto& engine = engineController.getEngine();
+    std::string methodKey = normalizeMethod(request.method().value()).value();
+    std::string path = normalizePath(request.path().value()).value();
+
+    // Find the handler for the requested path and method
+    auto methodIt = m_routes.find(methodKey);
+    if (methodIt != m_routes.end()) {
+        auto& routesByMethod = methodIt->second;
+
+        // Try to match the path with the registered routes
+        for (const auto& route : routesByMethod) {
+            const std::string& routePath = route.first;
+            std::smatch match;
+            std::regex routeRegex = createRouteRegex(routePath);
+
+            if (std::regex_match(path, match, routeRegex)) {
+                // Call the handler function and return its response
+                Response response = route.second(request);
+
+                // Apply middleware to the request and response
+                for (const auto& middleware : m_middleWares) {
+                    middleware(const_cast<Request&>(request), response, route.second);
+                }
+
+                return response;
+            }
+        }
+    }
+
+    // If no handler is found, call the custom error page handler if it's set
+    if (m_notFoundHandler) {
+        return m_notFoundHandler(request);
+    }
+
+    //! Todo for exception!?
+    // If no handler is found and no custom error page handler is set, return a default 404 response
+    Response response;
+    response.setStatusCode(404);
+    response.setContentType(engine.meta()->returnView(Globals::ContentTypes::HTML));
+    std::string r = R"(<html><body><h1>Default 404 Not Found</h1><p>The requested page was not found.</p></body></html>)";
+    response.setContent(r);
+    return response;
+}
+
+void Router::setNotFoundHandler(const Handler& notFoundHandler)
+{
+    m_notFoundHandler = notFoundHandler;
+}
+
+void Router::setExceptionHandler(const ExceptionErrorHandler& handler)
+{
+    m_exceptionHandler = handler;
+}
+
+OptionalString Router::normalizePath(const std::string& path)
+{
+    // Normalize the path by removing trailing slashes
+    std::string normalizedPath = path;
+    if (!normalizedPath.empty() && normalizedPath.back() == '/')
+    {
+        normalizedPath.pop_back();
+    }
+    return normalizedPath;
+}
+
+OptionalString Router::normalizeMethod(const std::string& method)
+{
+    // Normalize the method by converting to uppercase
+    std::string normalizedMethod = method;
+    std::transform(normalizedMethod.begin(), normalizedMethod.end(), normalizedMethod.begin(), ::toupper);
+    return normalizedMethod;
+}
+
+std::regex Router::createRouteRegex(const std::string& routePath) {
+    std::string regexPattern = "^" + std::regex_replace(routePath, std::regex(R"(\{(\w+)\})"), R"(([^/]+))") + "$";
+    return std::regex(regexPattern);
+}
+
+
+CELL_NAMESPACE_END
---
+++ source/modules/network/webserver/router.hpp
@@ -0,0 +1,135 @@
+/*!
+ * @file        router.hpp
+ * @brief       Router manager for the Cell Engine.
+ * @details     This file defines the Database interface, which provides methods for interacting with a database.
+ * @author      Kambiz Asadzadeh
+ * @since       07 Jun 2023
+ * @version     1.0
+ * @note        This is part of the Cell Engine, developed by Kambiz Asadzadeh.
+ *
+ * @license     This file is licensed under the terms of the Genyleap License. See the LICENSE.md file for more information.
+ * @copyright   Copyright (c) 2023 The Genyleap | Kambiz Asadzadeh. All rights reserved.
+ * @see         https://github.com/genyleap/cell
+ */
+
+#ifndef CELL_ROUTER_HPP
+#define CELL_ROUTER_HPP
+
+//! Cell's Core (Basic Requirements).
+#if __has_include(<requirements>)
+#   include <requirements>
+#else
+#   error "Cell's requirements was not found!"
+#endif
+
+
+#ifdef __has_include
+# if __has_include("request.hpp")
+#   include "request.hpp"
+#else
+#   error "Cell's "request.hpp" was not found!"
+# endif
+#endif
+
+#ifdef __has_include
+# if __has_include("response.hpp")
+#   include "response.hpp"
+#else
+#   error "Cell's "response.hpp" was not found!"
+# endif
+#endif
+
+CELL_NAMESPACE_BEGIN(Cell::Modules::BuiltIn::Network::WebServer)
+
+using Handler = std::function<Response(const Request&)>;
+using ExceptionErrorHandler = std::function<Response(const Request&, const std::exception&)>;
+using Middleware = std::function<void(Request&, Response&, const Handler&)>;
+/**
+ * Router class for handling HTTP route mapping and request routing.
+ */
+class __cell_export Router {
+public:
+    /**
+     * Add a route to the router.
+     *
+     * @param path The path for the route.
+     * @param handler The handler function for the route.
+     * @param method The HTTP method for the route (default: "GET").
+     */
+    void addRoute(const std::string& path, const Handler& handler, const std::string& method = "GET");
+
+    /**
+     * Add a middleware to the router.
+     *
+     * @param middleware The middleware function to add.
+     */
+    void addMiddleware(const Middleware& middleware);
+
+    /**
+     * Route the incoming request based on the registered routes and middleware.
+     *
+     * @param request The incoming request to route.
+     * @return The response generated from the routing process.
+     */
+    Response routeRequest(const Request& request);
+
+    /**
+     * @brief Sets the handler function for handling 404 Not Found errors.
+     *
+     * When a requested resource is not found, this handler function will be called to generate
+     * an appropriate response. The handler function should take a Request object as a parameter
+     * and return a Response object.
+     *
+     * @param errorHandler The handler function to set for handling 404 Not Found errors.
+     */
+    void setNotFoundHandler(const Handler& errorHandler);
+
+    /**
+     * @brief Sets the handler function for handling exceptions that occur during request handling.
+     *
+     * When an exception is thrown during request handling, this handler function will be called
+     * to handle the exception and generate an appropriate response. The handler function should
+     * take a const std::exception& object as a parameter and return a Response object.
+     *
+     * @param handler The handler function to set for handling exceptions.
+     */
+    void setExceptionHandler(const ExceptionErrorHandler& handler);
+
+
+private:
+    /**
+     * Normalize the path by removing trailing slashes and converting to lowercase.
+     *
+     * @param path The path to normalize.
+     * @return The normalized path.
+     */
+    Types::OptionalString normalizePath(const std::string& path);
+
+    /**
+     * Normalize the HTTP method by converting to uppercase.
+     *
+     * @param method The HTTP method to normalize.
+     * @return The normalized method.
+     */
+    Types::OptionalString normalizeMethod(const std::string& method);
+
+    /**
+     * Create a regular expression from the route path.
+     *
+     * @param routePath The route path to create the regex from.
+     * @return The created regex.
+     */
+    std::regex createRouteRegex(const std::string& routePath);
+
+    std::unordered_map<std::string, std::unordered_map<std::string, Handler>> m_routes;
+    std::vector<Middleware> m_middleWares;
+    Handler m_notFoundHandler;
+    ExceptionErrorHandler m_exceptionHandler;
+    System::EngineController engineController;
+};
+
+
+CELL_NAMESPACE_END
+
+#endif  // CELL_ROUTER_HPP
+
